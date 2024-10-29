9eb83d945bddf80ab81185d3d68f8574bbf87df5
Mikiyas N. <mikermount19@yahoo.com>
2024-10-29 17:06:07 +0300

    Update Feature2.jsx

--- src/Components/Feature2.jsx
+++ src/Components/Feature2.jsx
@@ -4,7 +4,7 @@ import Buttons from "./Buttons";
 const Feature2 = (props) => {
   return (
     <section className="flex flex-col justify-center items-center text-center text-white">
-      <div className="flex flex-col items-center mt-[15rem] sm:mt-[23rem]">
+      <div className="flex flex-col items-center mt-[12rem] sm:mt-[23rem]">
         <h1 className="text-4xl font-semibold sm:text-5xl">{props.title}</h1>
         <h2 className="mt-2 text-xl font-light sm:text-2xl">
           {props.subtitle}
